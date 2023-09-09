8290c7d4b864ebd571c9f2748abe980a6a6d03c2
Anderson-Duran <andersonduran@hotmail.com>
2023-09-09 11:10:47 -0300

    fix bugs

--- src/formularios/FormMedicacoes.jsx
+++ src/formularios/FormMedicacoes.jsx
@@ -33,7 +33,6 @@ export default function Medicines(props) {
 
                 if (!window.confirm('Deseja adicionar mais alguma medicação? Clique OK pra SIM, CANCEL pra NÃO')) {
                     window.location.href = 'https://vocal-granita-13bfe9.netlify.app';
-                    window.location.href = 'https://vocal-granita-13bfe9.netlify.app/cadastroPacientes'
                 }
             }
             return await response.json()
