a4f98ef536434a05c3665db7a17dc46fc569781e
Anderson-Duran <andersonduran@hotmail.com>
2023-09-15 02:40:55 -0300

    feat: up

--- src/telas/TelaCadMedicacoes.jsx
+++ src/telas/TelaCadMedicacoes.jsx
@@ -37,9 +37,7 @@ export default function TelaCadMedicacoes(props) {
             medicineDateEnd: location.state,
             medicineObservation: location.state
         })
-    } else {
-
-    }
+    } 
 
 
 
